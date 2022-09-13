28e1c0d6b9354956f368635da0c896b7943331f8
Antonius131 <antomendetta@gmail.com>
2022-09-13 16:52:53 +0200

    get all comics from db

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.main')
 
 @section('main-content')
-   <h4>comics list goes here</h4>
+   <table>
+      <tr>
+         <th>Id</th>
+         <th>Title</th>
+         <th>Series</th>
+         <th>Type</th>
+         <th>Price</th>
+      </tr>
+      <tr>
+         @forelse ($comics as $comic)
+            <td>{{ $comic->id }}</td>
+            <td>{{ $comic->title }}</td>
+            <td>{{ $comic->series }}</td>
+            <td>{{ $comic->type }}</td>
+            <td>{{ $comic->price }}</td>
+         @empty
+             <h2>Sorrry! No comics here...</h2>
+         @endforelse
+      </tr>
+   </table>
 @endsection
